c9a00d5e68fcd78d2da6a05def5d94884d702ad4
torrey <113153846+Toad-Soup@users.noreply.github.com>
2025-10-20 23:57:18 +0000

    tracked users mouse movements as points on an array, used Profs example code from quaint paint (step 3)

--- src/main.ts
+++ src/main.ts
@@ -12,23 +12,74 @@ canvas.width = 256;
 
 const cursor = { active: false, x: 0, y: 0 };
 
+const lines: { x: number; y: number }[][] = [];
+const redoLines: { x: number; y: number }[][] = [];
+
+let currentLine: { x: number; y: number }[] | null = null;
+
 canvas.addEventListener("mousedown", (e) => {
   cursor.active = true;
   cursor.x = e.offsetX;
   cursor.y = e.offsetY;
+
+  currentLine = [];
+  lines.push(currentLine);
+  currentLine.push({ x: cursor.x, y: cursor.y });
+  redoLines.splice(0, redoLines.length);
+  canvas.dispatchEvent(new Event("drawing-changed"));
 });
 
 canvas.addEventListener("mousemove", (e) => {
   if (cursor.active) {
-    ctx.beginPath();
-    ctx.moveTo(cursor.x, cursor.y);
-    ctx.lineTo(e.offsetX, e.offsetY);
-    ctx.stroke();
+    if (currentLine === null) {
+      return;
+    }
     cursor.x = e.offsetX;
     cursor.y = e.offsetY;
+
+    currentLine.push({ x: cursor.x, y: cursor.y });
+
+    canvas.dispatchEvent(new Event("drawing-changed"));
   }
 });
 
 canvas.addEventListener("mouseup", () => {
   cursor.active = false;
+  currentLine = null;
+
+  canvas.dispatchEvent(new Event("drawing-changed"));
+});
+
+//add the exception function thingy
+canvas.addEventListener("drawing-changed", () => {
+  redraw();
+});
+
+function redraw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (const line of lines) {
+    if (line.length > 1) {
+      ctx.beginPath();
+      const { x, y } = line[0];
+      ctx.moveTo(x, y);
+      for (const { x, y } of line) {
+        ctx.lineTo(x, y);
+      }
+      ctx.stroke();
+    }
+  }
+}
+
+document.body.append(document.createElement("br"));
+
+const undoButton = document.createElement("button");
+undoButton.innerHTML = "undo";
+document.body.append(undoButton);
+
+undoButton.addEventListener("click", () => {
+  const lastLine = lines.pop();
+  if (lastLine) {
+    redoLines.push(lastLine);
+    redraw();
+  }
 });
